73df2f4719c3154e969b2b69a89304a89869c207
Fernando chicaiza <washopilot@yahoo.com>
2022-05-04 12:14:19 -0500

    salió ptmr

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { request } from 'http';
-import React from 'react';
+import React, { Component } from 'react';
+import Fetch from 'react-fetch-component';
 import OData from 'react-odata';
 
-function App() {
-  const baseUrl = 'http://services.odata.org/V4/TripPinService/People';
-  const query = { filter: { FirstName: 'Russell' } };
+const baseUrl = 'http://services.odata.org/V4/TripPinService/People';
+const query = { filter: { FirstName: 'Russell' } };
 
-  return (
-    <OData baseUrl={baseUrl} query={query} options={{ mode: 'no-cors' }}>
-      {({ loading, error, data }: { loading: any; error: any; data: any }) => (
-        <div>
-          {loading && <span>Loading...</span>}
-          {data &&
-            data.value.map((d: any, i: any) => (
-              <div key={i} id={i}>
-                {d}
-              </div>
-            ))}
-        </div>
-      )}
-    </OData>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Basic</h1>
+        <OData baseUrl={baseUrl} query={query}>
+          {({ loading, data, error }: { loading: any; data: any; error: any }) => (
+            <div>
+              {loading && <span>Loading... </span>}
+              {data &&
+                data.value.map((d: any, i: any) => (
+                  <div key={i} id={i}>
+                    {d.FirstName}
+                  </div>
+                ))}
+            </div>
+          )}
+        </OData>
+      </div>
+    );
+  }
+
 }
-
-export default App;
